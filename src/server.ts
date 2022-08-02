a9ad4829516c5e5a1a496f8c1c63449d4d428c8f
Duke0404 <devansh0404@gmail.com>
2022-08-02 08:44:51 +0200

    Connected to mongodb

--- src/server.ts
+++ src/server.ts
@@ -2,65 +2,68 @@
 import express, { Express, Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
+//MongoDB
+import { MongoClient } from 'mongodb'
+
 //Iterfaces
 // interface storageTempInterface {
 // 	id: number
 // 	upvotes: number
 // }
 
-//Data
-const storageTemp: any = {
-	201901011245: {
-		upvotes: 0,
-		comments: [],
-	},
-	202001011246: {
-		upvotes: 0,
-		comments: [],
-	},
-	202201011247: {
-		upvotes: 0,
-		comments: [],
-	},
-	201401011248: {
-		upvotes: 0,
-		comments: [],
-	},
-}
-
 const app: Express = express()
 const port: number = 8000
 
 app.use(bodyParser.json())
 
-//Data
+//GET Routes
+app.get(
+	'/api/entries/:id',
+	async (req: Request, res: Response) => {
+		const entryId: number = +req.params.id
 
+		await MongoClient.connect(
+			'mongodb://localhost:27017',
+			async (error, client) => {
+				if (error || client === undefined)
+					res.status(500).json({ message: 'Error connecting to db', error })
+
+				else {	
+					const db = client.db('memories-db')
+					const entryInfo = await db.collection('entries').findOne({ id: entryId })
+					res.status(200).json(entryInfo)
+					client.close()
+				}
+			}
+		)
+	}
+)
 
 //POST Routes
-app.post(
-	'/api/entries/:id/upvote',
-	(req: Request, res: Response) => {
-		const entryId: number = parseInt(req.params.id)
+// app.post(
+// 	'/api/entries/:id/upvote',
+// 	(req: Request, res: Response) => {
+// 		const entryId: number = parseInt(req.params.id)
 
-		storageTemp[entryId].upvotes++
-		res.status(200).send(`Enrty now has ${storageTemp[entryId].upvotes} upvotes`)
-	}
-)
+// 		storageTemp[entryId].upvotes++
+// 		res.status(200).send(`Enrty now has ${storageTemp[entryId].upvotes} upvotes`)
+// 	}
+// )
 
-app.post(
-	'/api/entries/:id/add-comment',
-	(req: Request, res: Response) => {
-		const entryId: number = parseInt(req.params.id)
-		const userName: string = req.body.userName
-		const comment: string = req.body.comment
+// app.post(
+// 	'/api/entries/:id/add-comment',
+// 	(req: Request, res: Response) => {
+// 		const entryId: number = parseInt(req.params.id)
+// 		const userName: string = req.body.userName
+// 		const comment: string = req.body.comment
 
-		storageTemp[entryId].comments.push(
-			{ userName, comment }
-		)
+// 		storageTemp[entryId].comments.push(
+// 			{ userName, comment }
+// 		)
 
-		res.status(200).send('Comment added')
-	}
-)
+// 		res.status(200).send('Comment added')
+// 	}
+// )
 
 app.listen(
 	port,

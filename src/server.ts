8bc3ebac3135cefff25898a6a86202dcfe54ac8d
Duke0404 <devansh0404@gmail.com>
2022-08-06 19:22:05 +0200

    Fixed MongoDB connection

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import { MongoClient, Db, Collection, WithId, Document } from 'mongodb'
 // Constant to store express object
 const app: Express = express()
 // Constant to store MongoDB URI
-const mongoURI: string = 'mongodb://localhost:27017'
+const mongoURI: string = 'mongodb://127.0.0.1:27017'
 // Constant to store port number to listen on
 const port: number = 8000
 
@@ -21,7 +21,6 @@ app.get(
 
 		MongoClient.connect(
 			mongoURI,
-			{},
 			async (error, client) => {
 				if (error || !client)
 					res.status(500).json({ message: 'Error connecting to db', error })
@@ -40,30 +39,46 @@ app.get(
 	}
 )
 
-// const client: MongoClient = new MongoClient(mongoURI)
+/*******************************************/
+// 201901011245
 
-// async function run() {
+// const listDatabases = async (client: MongoClient) => {
+// 	const databasesList = await client.db().admin().listDatabases()
+
+// 	console.log("Databases:")
+
+// 	databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+// }
+
+// const main = async () => {
+// 	const client: MongoClient = new MongoClient(mongoURI)
+
+
 // 	try {
 // 		await client.connect()
 
-//     // database and collection code goes here
-// 		const db: Db = client.db('memories-db')
-// 		const collection: Collection<Document> = db.collection('entries')
+// 		await listDatabases(client)
+// 	}
 
-//     // find code goes here
-// 		const entryInfo: WithId<Document> | null = await collection.findOne({ id: 201401011248 })
-		
-//     // iterate code goes here
-
+// 	catch (error) {
+// 		console.log(error)
 // 	}
 
 // 	finally {
-// 		await client.close();
+// 		await client.close()
 // 	}
-
 // }
 
-// run().catch(console.dir);
+// main().catch(console.error)
+
+/*******************************************/
+
+app.get(
+	"/api/test",
+	(req: Request, res: Response) => {
+		res.status(200).send("Hello World")
+	}
+)
 
 app.listen(
 	port,
